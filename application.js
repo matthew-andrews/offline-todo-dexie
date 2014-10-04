d9a41dd2b2d2eebc301e92bf9f07f78eaebccc2d
Matthew Andrews <matt@mattandre.ws>
2014-10-04 21:31:52 +0100

    Namespace DB

--- application.js
+++ application.js
@@ -50,7 +50,7 @@
   function databaseOpen() {
     return new Promise(function(resolve, reject) {
       var version = 1;
-      var request = indexedDB.open('todos', version);
+      var request = indexedDB.open('todos-dexie', version);
       request.onupgradeneeded = function(e) {
         db = e.target.result;
         e.target.transaction.onerror = reject;
